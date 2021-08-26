b7c3a0c38b2a72dff77ad9e7398755a2e61fc838
Kristian Nissen <kristian.nissen@gmail.com>
2021-08-26 16:56:43 +0200

    Almost there

--- js/form.js
+++
@@ -1,8 +0,0 @@
-/**/
-(function(self) {
-  var f = self.document.querySelector("form");
-
-  f.addEventListener("submit", function(evnt) {
-    f.csrftoken.value = window._CSRF;
-  }); 
-})(this);
---
+++ js/viewport.js
@@ -0,0 +1,22 @@
+/* viewport.js */
+
+function onScrollStop(cb) {
+	var s;
+	window.addEventListener('scroll', function(evnt) {
+		window.clearTimeout(s);
+
+		s = setTimeout(function() {
+			cb()
+		}, 100)
+	})
+}
+
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
