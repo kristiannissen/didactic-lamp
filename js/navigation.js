3294873f9dcaf5c089cae5866a9356d818a894fe
Kristian Nissen <kristian.nissen@gmail.com>
2021-09-08 17:38:09 +0200

    Folder structure and navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-/* Navigation */
+/* Navigation for mobile */
 document.addEventListener("DOMContentLoaded", function() {
 	var b = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 	if (b.length > 0) {
@@ -10,4 +10,25 @@ document.addEventListener("DOMContentLoaded", function() {
 			})
 		})
 	}
+})
+
+// jQuery navigation pushstate
+function pushPage(uri) {
+	$('#dyn-main').load(uri +' #dyn-main');
+	window.history.pushState({href: uri}, '', uri)
+}
+
+$(document).ready(function() {
+	$('.navbar-item').on('click', function(evnt) {
+		var navItem = $(evnt.target)
+		if (navItem.attr('href') === undefined) {
+			navItem = navItem.parent();
+		}
+
+		pushPage(navItem.attr('href'));
+		return false;
+	})
+	window.addEventListener('popstate', function(evnt) {
+		if (evnt.state) pushPage($(evnt.target).attr('href'));
+	})
 })
